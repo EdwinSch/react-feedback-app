24368f8129ef7c94e25a58a52f25fc9852f965d3
EdwinSch <edwinschutjes@gmail.com>
2023-11-17 14:46:32 +0100

    add: de;ete item funcions

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@ import { useState } from "react";
 function App() {
   const [feedback, setFeedback] = useState(feedbackData);
 
+  const deleteFeedback = (id) => {
+    if (window.confirm("This action will delete this post!")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,13 @@
 import Card from "./shared/Card";
+import { FaTimes } from "react-icons/fa";
 
-const FeedbackItem = ({ id, rating, text }) => {
+const FeedbackItem = ({ id, rating, text, handleDelete }) => {
   return (
     <Card>
       <div className="num-display">{rating}</div>
+      <button onClick={() => handleDelete(id)} className="close" type="button">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{text}</div>
     </Card>
   );
